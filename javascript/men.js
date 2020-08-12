8000d122d21d86902ff5c0eb5fea6e2c2887d3e2
brianalterescu <btopcualterescu@gmail.com>
2020-08-12 12:41:58 -0400

    Change some visuals and updated backend code for the API.

---
+++ javascript/men.js
@@ -0,0 +1,74 @@
+function searchItem(search) {
+    search = encodeURIComponent(search);
+
+    let proxy = 'https://cors-anywhere.herokuapp.com/';
+    let appID = 'KennethM-Scenti-PRD-3c8eaa0db-1c03fd2e';
+    let url = proxy + `https://open.api.ebay.com/shopping?callname=FindProducts&responseencoding=JSON&appid=${appID}&siteid=0&QueryKeywords=${search}&version=1157&MaxEntries=5`;
+
+    let result;
+    fetch(url)
+        .then(function (response) {
+            return response.json();
+
+        })
+        .then(function (json) {
+            console.log(json);
+            displayData(json);
+
+        })
+}
+searchItem("cologne");
+
+let container = document.getElementById("mens-container");
+
+function displayData(json) {
+
+
+    for (i = 0; i < json.Product.length; i++) {
+        console.log(json.Product[i].Title);
+
+        json.Product[i].DisplayStockPhotos = true
+        let item_name = json.Product[i].Title;
+        let item_image = json.Product[i].StockPhotoURL;
+
+        let div = document.createElement("div");
+
+        let img = document.createElement("img");
+        img.src = item_image;
+        div.appendChild(img);
+        
+        let p = document.createElement("p");
+        p.innerText = item_name;
+        div.appendChild(p);
+        
+        container.appendChild(div);
+    }
+
+
+
+
+
+        //PRODUCT 1
+        // let item_name1 = json.Product[0].Title;
+        // let item_image1 = json.Product[0].StockPhotoURL;
+
+        // let img = document.createElement("img");
+        // img.src = item_image1;
+        // document.body.appendChild(img);
+
+        // let div = document.createElement("div")
+        // div.innerText = item_name1;
+        // document.body.appendChild(div);
+        // //PRODUCT 1
+
+        // //PRODUCT 2
+        // let item_name2 = json.
+        // let item_image2 = json.Product[1].StockPhotoURL;
+    
+        // let img1 = document.createElement("img")
+
+        // json.Product[4].DisplayStockPhotos = true
+    // }
+
+}
+//rearrange the img and div so the img can come first and then the name of the product can appear//
---
+++ javascript/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(2500, 65);
+    background(250, 11, 83);
+    strokeWeight(4);
+    stroke(51);
+
+    noStroke();
+    let s = 'Scentient';
+    fill(255, 255, 255);
+    textSize(32);
+    textFont('Pacifico');
+    text(s, 550, 20, 10, 60)
+  
+}
